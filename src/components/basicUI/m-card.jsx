ef7b8dae7ec69d6c9fde942f1dbb97892b7e370b
Duchai232 <Duchai757@gmail.com>
2025-02-03 16:11:02 +0700

    useEffect useState

--- src/components/basicUI/m-card.jsx
+++ src/components/basicUI/m-card.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { Card } from "antd";
 import Meta from "antd/es/card/Meta";
-const MCard = ({ title, description, number }) => {
+const MCard = ({ title, description, number, image }) => {
   return (
     <Card
       hoverable
       style={{ width: 240 }}
-      cover={
-        <img
-          alt="example"
-          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-        />
-      }
+      cover={<img alt="example" src={image} />}
     >
       <Meta title={title} description={description} />
       {number}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MCard from "../components/basicUI/m-card";
-
+import { Button, Col, message, Row, Spin } from "antd";
+import axios from "axios";
+//interface, type
 const Home = () => {
+  const [dataService, setDataService] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const asyncLoadData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        "https://swd-hair-be.onrender.com/view-service"
+      );
+      message.success("Tải thành công");
+      setDataService(response.data.services);
+    } catch (error) {
+      message.error("Lỗi hệ thống");
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    asyncLoadData();
+  }, []);
   return (
-    <div>
-      <MCard title="ABC" description="CDE" number="10" />
-      <MCard title="EDF" description="MLN" />
-    </div>
+    <Spin spinning={loading}>
+      <Row>
+        {dataService.map((item) => (
+          <Col span={6}>
+            <MCard title={item.name} number={item.price} image={item.image} />
+          </Col>
+        ))}
+      </Row>
+    </Spin>
   );
 };
 
